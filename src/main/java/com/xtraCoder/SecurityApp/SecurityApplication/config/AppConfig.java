3a82e6f257e6549f0b8c06bb95b28e183427217d
Devbrat Pradhan <devbratpradhan40@gmail.com>
2025-10-14 00:47:22 +0530

    updated security

--- src/main/java/com/xtraCoder/SecurityApp/SecurityApplication/config/AppConfig.java
+++ src/main/java/com/xtraCoder/SecurityApp/SecurityApplication/config/AppConfig.java
@@ -3,10 +3,8 @@ package com.xtraCoder.SecurityApp.SecurityApplication.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing(auditorAwareRef = "getAuditorAware") // Enable JPA Auditing for automatic timestamp management
 public class AppConfig {
 
     @Bean
